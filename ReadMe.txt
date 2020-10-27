5d12d051a4554707ac06b1437c8923cbdbcfa7fc
Calvin Buckley <calvin@cmpct.info>
2020-10-26 23:19:00 -0300

    initial commit

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+timer is like unix's time shell built-in. shove the full command line
+into the argument for timer and it'll calculate the time elapsed, and
+if possible (on NT), system and user time.
---
+++ timer.cpp
@@ -0,0 +1,121 @@
+// timer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+void ft2ul(LPFILETIME ft, PULARGE_INTEGER ul)
+{
+	ul->LowPart = ft->dwLowDateTime;
+	ul->HighPart = ft->dwHighDateTime;
+}
+
+void ul2ft(PULARGE_INTEGER ul, LPFILETIME ft)
+{
+	ft->dwLowDateTime = ul->LowPart;
+	ft->dwHighDateTime = ul->HighPart;
+}
+
+char *stime2str(LPSYSTEMTIME st)
+{
+	// XXX: This is likely extremely imprecise and breaks past days
+	// XXX: also very bad strcats
+	char str[1024], tmp[128];
+	ZeroMemory(str, 1024);
+	ZeroMemory(tmp, 128);
+	if (st->wDay - 1) {
+		// SYSTEMTIMEs default at 1
+		sprintf(tmp, "%dd", st->wDay - 1);
+		strcat(str, tmp);
+	}
+	if (st->wMinute) {
+		sprintf(tmp, "%dm", st->wMinute);
+		strcat(str, tmp);
+	}
+	// always print seconds, but maybe not ms
+	sprintf(tmp, "%d", st->wSecond);
+	strcat(str, tmp);
+	if (st->wMilliseconds) {
+		sprintf(tmp, ".%d", st->wMilliseconds);
+		strcat(str, tmp);
+	}
+	strcat(str, "s");
+	return strdup(str);
+}
+
+char *ftime2str(LPFILETIME ft)
+{
+	SYSTEMTIME st;
+	FileTimeToSystemTime(ft, &st);
+	return stime2str(&st);
+}
+
+TCHAR *tcscomb(int argc, TCHAR *argv[])
+{
+	TCHAR str[32768];
+	ZeroMemory(str, 32768);
+	for (int i = 0; i < argc; i++) {
+		if (i)
+			_tcscat(str, " ");
+		_tcscat(str, argv[i]);
+	}
+	return _tcsdup(str);
+}
+
+int _tmain(int argc, TCHAR *argv[])
+{
+	BOOL ok;
+	STARTUPINFO sinfo;
+	PROCESS_INFORMATION pinfo;
+	char *strReal, *strSys, *strUser, *combinedArgv;
+	FILETIME beforeCreate, creation, ending, real, kernel, user;
+	ULARGE_INTEGER ulCreate, ulEnd, ulReal;
+
+	if (argc < 2) {
+		fputs("you must specify a command line\n", stderr);
+		return 1;
+	}
+
+	combinedArgv = tcscomb(argc - 1, argv + 1);
+	ZeroMemory(&sinfo, sizeof(sinfo));
+	ZeroMemory(&pinfo, sizeof(pinfo));
+
+	GetSystemTimeAsFileTime(&beforeCreate);
+	ok = CreateProcess(NULL, combinedArgv, NULL, NULL, TRUE,
+		0, NULL, NULL, &sinfo, &pinfo);
+	if (!ok) {
+		_ftprintf(stderr, _T("timer failed to create process of: %s\n"), argv[1]);
+	}
+	WaitForSingleObject(pinfo.hProcess, INFINITE);
+
+	if (GetProcessTimes(pinfo.hProcess, &creation, &ending, &kernel, &user)) {
+		// NT
+		ft2ul(&creation, &ulCreate);
+		ft2ul(&ending, &ulEnd);
+		ulReal.QuadPart = ulEnd.QuadPart - ulCreate.QuadPart;
+		ul2ft(&ulReal, &real);
+
+		strReal = ftime2str(&real);
+		strSys = ftime2str(&kernel);
+		strUser = ftime2str(&user);
+		printf("real %s sys %s user %s\n", strReal, strSys, strUser);
+		free(strReal);
+		free(strSys);
+		free(strUser);
+	} else {
+		// 9x
+		GetSystemTimeAsFileTime(&ending);
+		ft2ul(&beforeCreate, &ulCreate);
+		ft2ul(&ending, &ulEnd);
+		ulReal.QuadPart = ulEnd.QuadPart - ulCreate.QuadPart;
+		ul2ft(&ulReal, &real);
+
+		strReal = ftime2str(&real);
+		printf("real %s\n", strReal);
+		free(strReal);
+	}
+	CloseHandle(pinfo.hProcess);
+	CloseHandle(pinfo.hThread);
+
+	return 0;
+}
+
